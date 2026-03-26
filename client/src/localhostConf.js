2808606698da35b4464ac68e7d4ef3cae5e8384c
Ante Melvan <antemelvan123@gmail.com>
2026-03-26 19:45:15 +0100

    mi

--- client/src/localhostConf.js
+++ client/src/localhostConf.js
@@ -6,9 +6,9 @@ const productionUrlBackup = process.env.REACT_APP_VERCEL_URL;
 export const backendUrl =
   mode === "production"
     ? productionUrl
-    : "http://10.113.155.14:3000";
+    : "https://server-test-blue.vercel.app";
 
 export const backendUrlBackup =
   mode === "production"
     ? productionUrlBackup
-    : "http://10.99.86.14:3000";
+    : "https://server-test-blue.vercel.app";

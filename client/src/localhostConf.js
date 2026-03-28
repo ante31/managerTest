129bf5017658c908bed635cdb3dfadae1973db5a
ante31 <92745972+ante31@users.noreply.github.com>
2026-03-28 13:45:13 +0100

    Update localhostConf.js

--- client/src/localhostConf.js
+++ client/src/localhostConf.js
@@ -6,9 +6,9 @@ const productionUrlBackup = process.env.REACT_APP_VERCEL_URL;
 export const backendUrl =
   mode === "production"
     ? productionUrl
-    : "https://server-test-blue.vercel.app";
+    : "https://servertest-production-dc6f.up.railway.app";
 
 export const backendUrlBackup =
   mode === "production"
     ? productionUrlBackup
-    : "https://server-test-blue.vercel.app";
+    : "https://servertest-production-dc6f.up.railway.app";
